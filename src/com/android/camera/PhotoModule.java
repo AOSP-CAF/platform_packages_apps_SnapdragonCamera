31e33b3207de3337dfb7c055cddbe7abcce7e0f6
codeworkx <codeworkx@cyanogenmod.org>
2016-01-06 00:10:40 +0100

    add options to restart preview onPictureTaken
     [AdrianDC] Adapt for duplicated info variable
    
    Change-Id: I7ce9d6f2323e367743e6d4c9662b846d3052f2a4

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -68,6 +68,7 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.android.camera.app.CameraApp;
 import com.android.camera.CameraManager.CameraAFCallback;
 import com.android.camera.CameraManager.CameraAFMoveCallback;
 import com.android.camera.CameraManager.CameraPictureCallback;
@@ -266,6 +267,8 @@ public class PhotoModule
 
     private byte[] mLastJpegData;
     private int mLastJpegOrientation = 0;
+
+    private static Context mApplicationContext;
 
     private Runnable mDoSnapRunnable = new Runnable() {
         @Override
@@ -937,6 +940,8 @@ public class PhotoModule
             return;
         }
 
+        mApplicationContext = CameraApp.getContext();
+
         // Initialize location service.
         boolean recordLocation = RecordLocationPreference.get(mPreferences,
                 CameraSettings.KEY_RECORD_LOCATION);
@@ -1401,6 +1406,18 @@ public class PhotoModule
                     && (mCameraState != LONGSHOT)
                     && (mSnapshotMode != CameraInfo.CAMERA_SUPPORT_MODE_ZSL)
                     && (mReceivedSnapNum == mBurstSnapNum);
+
+            boolean backCameraRestartPreviewOnPictureTaken = 
+                mApplicationContext.getResources().getBoolean(R.bool.back_camera_restart_preview_onPictureTaken);
+            boolean frontCameraRestartPreviewOnPictureTaken = 
+                mApplicationContext.getResources().getBoolean(R.bool.front_camera_restart_preview_onPictureTaken);
+
+            CameraInfo info = CameraHolder.instance().getCameraInfo()[mCameraId];
+            if ((info.facing == CameraInfo.CAMERA_FACING_BACK && backCameraRestartPreviewOnPictureTaken) 
+                || (info.facing == CameraInfo.CAMERA_FACING_FRONT && frontCameraRestartPreviewOnPictureTaken)) {
+                needRestartPreview = true;
+            }
+
             if (needRestartPreview) {
                 setupPreview();
                 if (CameraUtil.FOCUS_MODE_CONTINUOUS_PICTURE.equals(
@@ -1441,7 +1458,7 @@ public class PhotoModule
                             .findPreference(CameraSettings.KEY_SELFIE_MIRROR);
                     if (selfieMirrorPref != null && selfieMirrorPref.getValue() != null &&
                             selfieMirrorPref.getValue().equalsIgnoreCase("enable")) {
-                        CameraInfo info = CameraHolder.instance().getCameraInfo()[mCameraId];
+                        info = CameraHolder.instance().getCameraInfo()[mCameraId];
                         jpegData = flipJpeg(jpegData, info.orientation, orientation);
                         jpegData = addExifTags(jpegData, orientation);
                     }
